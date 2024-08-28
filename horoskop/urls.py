dcfad2755340025f9067695217d72899aab64132
Andrej <533737@gmail.com>
2024-08-28 13:19:26 +0200

    prvi

--- horoskop/urls.py
+++ horoskop/urls.py
@@ -5,7 +5,7 @@ from .views import home, horoskop, prikazi_znak
 urlpatterns = [
     path('', home, name='home'),
 
-    path('<str:znak>/', horoskop, name='horoskop'),
+    path('<str:znak>', horoskop, name='horoskop'),
     path('znak/<str:znak>/', prikazi_znak, name='prikazi_znak'),
 
 ]
